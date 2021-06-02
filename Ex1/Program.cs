2597f68be0a1a66ecd75ee2f8773a7ea0cfcc817
Quynh anh <71160175+Quynhanh4622@users.noreply.github.com>
2021-06-02 13:37:14 +0700

    upload labs1

---
+++ Ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using Ex1.entity;
+namespace Ex1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("vui lòng nhập thông tin:");
+            SinhVien sinhVien = new SinhVien();
+            Console.WriteLine("Nhập tên: ");
+            sinhVien.Name = Console.ReadLine();
+            Console.WriteLine("Nhập địa chỉ: ");
+            sinhVien.Address = Console.ReadLine();
+            Console.WriteLine("Nhập số điện thoại: ");
+            sinhVien.Phone = Console.ReadLine();
+        }
+    }
+}
---
+++ Ex1/entity/SinhVien.cs
@@ -0,0 +1,9 @@
+﻿namespace Ex1.entity
+{
+    internal class SinhVien
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
