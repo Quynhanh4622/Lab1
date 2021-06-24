0da72056ba7ddf14a468df6c69bbe36ff67653b9
Quynh anh <71160175+Quynhanh4622@users.noreply.github.com>
2021-06-24 19:53:34 +0700

    lab8

---
+++ lab8.ex2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace lab8.ex2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var random = new Random();
+            var temperature = new double[300];
+            for (var i = 0; i < 300; i++)
+            {
+                temperature[i] = random.Next(0, 1000);
+            }
+
+            while (true)
+            {
+                Console.Write("vui lòng nhập vào số min : ");
+                var min = int.Parse(Console.ReadLine());
+                Console.WriteLine($"GreaterCount trả về : {GreaterCount(temperature, min)} lần temperature lớn hơn số min đã nhập : {min}\n\n");
+            }
+        }
+
+        private static int GreaterCount(IEnumerable enumerable, double min)
+        {
+            var count = 0;
+            foreach (var item in enumerable)
+            {
+                if (double.Parse(item.ToString()) >= min)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
